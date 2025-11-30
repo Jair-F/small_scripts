5276b0911d27df509195937e6c9923e67a55620a
Jair-F <jair.fehlauer@gmail.com>
2025-11-30 13:39:13 +0200

    made remove 100mb.sh like add

--- transfer/remove_100mb.sh
+++ transfer/remove_100mb.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-echo "filename: "
-read orig_file
-echo "output_file: "
-read out_file
+if [ -z "$1" ]; then
+    echo -n "Input File: "
+    read INPUT_FILE
 
-dd if=$orig_file of=$out_file bs=1M skip=100
+    echo -n "Num of KB to add(empty=4): "
+    read NUM_OF_KBYTES
+else
+    INPUT_FILE=$1
+    NUM_OF_KBYTES=$2
+fi
+
+if [ -z "$NUM_OF_KBYTES" ]; then
+    NUM_OF_KBYTES="4"
+
+    echo "going with default bytes to remove: $NUM_OF_KBYTES kb"
+fi
+
+# removing trailing and leading ', "
+INPUT_FILE="${INPUT_FILE//\"/}"
+INPUT_FILE="${INPUT_FILE//\'/}"
+
+# remove .bin at the end
+OUTPUT_FILE="${INPUT_FILE%.bin}"
+
+dd if=$INPUT_FILE of=$OUTPUT_FILE bs=1K skip=$NUM_OF_KBYTES
