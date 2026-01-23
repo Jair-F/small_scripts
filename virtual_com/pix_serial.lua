8d424b37d6862859eeb382c19f490050e3887aa1
Jair-F <jair.fehlauer@gmail.com>
2026-01-23 20:09:32 +0200

    little refactor

--- virtual_com/pix_serial.lua
+++ virtual_com/pix_serial.lua
@@ -1,22 +1,24 @@
--- local mavlink_msgs = require("MAVLink/mavlink_msgs")
 
 local PAYLOAD_CUSTOM_ID = 40002
-local MAV_TUNNEL_ID = 385
--- local MAV_TUNNEL_ID = mavlink_msgs.get_msgid("TUNNEL")
+local TARGET_SYSTEM = param:get("MAV_GCS_SYSID") -- gcs Station
+local TARGET_COMPONENT = param:get("MAV_SYSID") -- autopilot
+local MAV_TUNNEL_ID = 385 -- https://mavlink.io/en/messages/common.html#TUNNEL
 
-local function send_tunnel_msg(chan)
-    local target_system = 255
-    local target_component = 1
+local function send_tunnel_msg(mav_channel, payload_binary)
+    if mav_channel == nil then
+       return 
+    end
+
+    local MAV_TUNNEL_ID = 385 -- https://mavlink.io/en/messages/common.html#TUNNEL
+    local target_system = TARGET_SYSTEM
+    local target_component = TARGET_COMPONENT
     local payload_type = PAYLOAD_CUSTOM_ID
 
-    -- 2. Create the data payload (binary string)
-    -- Pack two floats (8 bytes total)
-    local data_bytes = string.pack("<ff", 12.34, 56.78)
-    local data_len = #data_bytes
+    local payload_len = #payload_binary
 
     -- 3. Pad to exactly 128 bytes (MAVLink Tunnel requirement)
-    -- We take our 8 bytes and add 120 zero-bytes
-    local full_payload = data_bytes .. string.rep(string.char(0), 128 - data_len)
+    -- filling up remaining junk data
+    local full_payload = payload_binary .. string.rep(string.char(0), 128 - payload_len)
 
     -- 3. Pack the message fields into a MAVLink binary string
     -- Format string "<HBBBc128" means:
@@ -26,16 +28,15 @@ local function send_tunnel_msg(chan)
     -- B    : uint8  (target_component)
     -- B    : uint8  (payload_length)
     -- c128 : 128 fixed-length characters (the payload)
-    local packed_msg = string.pack("<HBBBc128", 
-        payload_type, 
-        target_system, 
-        target_component, 
-        data_len, 
+    local packed_msg = string.pack('<HBBBc128',
+        payload_type,
+        target_system,
+        target_component,
+        payload_len,
         full_payload
     )
-    mavlink:send_chan(chan, MAV_TUNNEL_ID, packed_msg)
 
-    gcs:send_text('6', "Sent Tunnel Data to Python")
+    mavlink:send_chan(mav_channel, MAV_TUNNEL_ID, packed_msg)
 end
 local function extract_tunnel_data(raw_string)
     if not raw_string or #raw_string < 15 then return nil end
@@ -44,7 +45,7 @@ local function extract_tunnel_data(raw_string)
     -- The Message ID for TUNNEL (385) in hex is 0x000181
     -- In the wire stream, this appears as 81 01 00
     local start_idx = raw_string:find("\129\1\0") 
-    
+
     if not start_idx then
         return nil -- Message ID not found in this packet
     end
@@ -64,28 +65,28 @@ local function extract_tunnel_data(raw_string)
     local actual_payload = p_buffer
     -- local actual_payload = string.sub(p_buffer, 1, p_len)
 
-    return p_type, actual_payload, p_len
+    return p_type, actual_payload, p_len, target_sys, target_comp
 end
 
 local function update()
     gcs:send_text('7', "trying to receive mavlink msg")
     local raw_data, chan, recv_time = mavlink:receive_chan()
     gcs:send_text('7', 'chan:' .. tostring(chan))
-    send_tunnel_msg(0)
+    send_tunnel_msg(chan, string.pack("<ff", 12.34, 56.78))
 
     if raw_data == nil then
         gcs:send_text('7', "msg is nil")
         return update, 1000
     end
 
-    local p_type, actual_payload, p_len = extract_tunnel_data(raw_data)
+    local p_type, actual_payload, p_len, target_sys, target_comp = extract_tunnel_data(raw_data)
 
     if p_type then
         gcs:send_text(7, "payload_type: " .. tostring(p_type))
         gcs:send_text(7, "payload len: " .. tostring(p_len))
 
         local status, b1, b2, b3, b4, b5, b6, b7 = pcall(string.unpack, "<BBBBBBB", actual_payload)
-    
+
         if status then
             -- This prints a single clean string to the GCS
             gcs:send_text(6, string.format("Hex: %02X %02X %02X %02X %02X %02X %02X", b1, b2, b3, b4, b5, b6, b7))
@@ -98,7 +99,7 @@ local function update()
     --     gcs:send_text("7", 'got tunnel message from mav')
     --     if msg.payload_type == PAYLOAD_CUSTOM_ID then
     --         gcs:send_text("7", "got message with matching paylod type!")
-            
+    
     --         local len = msg:get_field('payload_length')
     --         local data = msg:get_field('payload')
 
