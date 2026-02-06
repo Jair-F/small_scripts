e2be5e4d1766abe6f1fdbd30bbfb5395e65986b0
Jair-F <jair.fehlauer@gmail.com>
2026-02-06 15:58:31 +0200

    linux - added saving how much bytes added in file itself for removing data

--- transfer/add_data.sh
+++ transfer/add_data.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
 TEMP_FILE="/tmp/tmp_random_data.bin"
+MAGIC_BITS_OFFSET_BYTES=32
+
+appendRandomData() {
+    local fileName="$1"
+    local numOfBytes=$2
+    dd if=/dev/urandom bs=1 count=$numOfBytes status=none >> $fileName
+}
 
 if [ -z "$1" ]; then
     echo -n "Input File: "
     read INPUT_FILE < /dev/tty
 
-    echo -n "Num of KB to add(empty=4): "
+    echo -n "Num of KB to add(empty=4, max=99): "
     read NUM_OF_KBYTES < /dev/tty
 else
     INPUT_FILE=$1
     NUM_OF_KBYTES=$2
 fi
 
-if [ -z "$NUM_OF_KBYTES" ]; then
+if [ -z "$NUM_OF_KBYTES" ] || [ $NUM_OF_KBYTES -lt 1 ]; then
     NUM_OF_KBYTES="4"
     echo "going with default bytes to add: $NUM_OF_KBYTES kb"
-else
-    echo "set bytes to add to : $NUM_OF_KBYTES kb"
+fi
+
+if [ $NUM_OF_KBYTES -gt 99 ]; then
+    NUM_OF_KBYTES="99"
+fi
+echo "set bytes to add to : $NUM_OF_KBYTES kb"
+if [ "$NUM_OF_KBYTES" -lt 10 ] && [ ${#NUM_OF_KBYTES} -lt 2 ]; then
+    NUM_OF_KBYTES="0$NUM_OF_KBYTES"
 fi
 
 # removing trailing and leading ', "
 INPUT_FILE="${INPUT_FILE//\"/}"
 INPUT_FILE="${INPUT_FILE//\'/}"
-
 OUTPUT_FILE="$INPUT_FILE.bin"
 
-touch /tmp/tmp_random_data.bin
+touch $TEMP_FILE
+truncate -s 0 $TEMP_FILE
 
-dd if=/dev/urandom of=$TEMP_FILE bs=1K count=$NUM_OF_KBYTES
+appendRandomData "$TEMP_FILE" "$MAGIC_BITS_OFFSET_BYTES"
+
+echo "$NUM_OF_KBYTES" >> $TEMP_FILE
+
+appendRandomData "$TEMP_FILE" $(( $NUM_OF_KBYTES * 1024 ))
 cat $TEMP_FILE $INPUT_FILE > $OUTPUT_FILE
 rm $TEMP_FILE
--- transfer/remove_data.sh
+++ transfer/remove_data.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+MAGIC_BITS_OFFSET_BYTES=32
+
 if [ -z "$1" ]; then
     echo -n "Input File: "
     read INPUT_FILE < /dev/tty
-
-    echo -n "Num of KB to add(empty=4): "
-    read NUM_OF_KBYTES < /dev/tty
 else
     INPUT_FILE=$1
-    NUM_OF_KBYTES=$2
-fi
-
-if [ -z "$NUM_OF_KBYTES" ]; then
-    NUM_OF_KBYTES="4"
-
-    echo "going with default bytes to remove: $NUM_OF_KBYTES kb"
 fi
 
 # removing trailing and leading ', "
 INPUT_FILE="${INPUT_FILE//\"/}"
 INPUT_FILE="${INPUT_FILE//\'/}"
 
+NUM_OF_KBYTES=$(dd if=$INPUT_FILE bs=1 skip=32 count=2 status=none | tr -dc '0-9')
+
+if [ -z "$NUM_OF_KBYTES" ]; then
+    echo "Error: Could not find a valid number at offset 32"
+    exit 1
+fi
+
+TOTAL_SKIP_BYTES=$(( (NUM_OF_KBYTES * 1024) + MAGIC_BITS_OFFSET_BYTES + 3 ))
+
 # remove .bin at the end
 OUTPUT_FILE="${INPUT_FILE%.bin}"
 
-dd if=$INPUT_FILE of=$OUTPUT_FILE bs=1K skip=$NUM_OF_KBYTES
+dd if=$INPUT_FILE of=$OUTPUT_FILE bs=1 skip=$TOTAL_SKIP_BYTES
