47640fba85bd99dc15c28dc9e6b0cf74ab4271bd
Jair-F <jair.fehlauer@gmail.com>
2026-07-13 11:00:14 +0300

    Create setup_ca.sh to install programs and extensions
    This script installs essential programs and VS Code extensions for development.

---
+++ setup_ca.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+install_programs() {
+    CURRENT_USER=$1
+    echo "User which started the script: $CURRENT_USER"
+    echo "Current user privilege is: $(whoami)"
+
+    snap install --classic code
+    apt update
+    apt install -y git wget curl docker.io docker-compose-v2 docker-buildx mono-complete
+    usermod -aG docker $CURRENT_USER
+    chmod 666 /var/run/docker.sock
+}
+
+sudo bash -c "$(declare -f install_programs); install_programs $USER"
+
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-python.python
+code --install-extension PKief.material-icon-theme
